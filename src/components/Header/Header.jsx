285b52027fd5c4cc65de266870d3d3a6bf1e83e3
Tiaguh <tiagosingup@gmail.com>
2023-07-24 17:53:27 -0300

    fixing carrousel bugs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 import "./Header.css"
 
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import navbar from './img/navbar.png';
 
